033f1e1b801d17b6c67328a2316decc1a3caefcb
Tholumuzi Kuboni <tholumuzi@inandi.co.za>
2024-04-09 16:38:38 +0000

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,6 +17,10 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 
 		{
+			for (j = 0; j <= n; j++)
+
+			{
+
 			k = j * i;
 			if (j == 0)
 
@@ -60,7 +64,9 @@ void print_times_table(int n)
 
 		}
 
-		_putchar('\n');
+		_putchar('\n')
+
+		}
 
 	}
 
