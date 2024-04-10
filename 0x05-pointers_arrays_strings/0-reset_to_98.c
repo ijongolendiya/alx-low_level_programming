b1e035f5540f4859180ae8af1f7ee6802f35a48a
Tholumuzi Kuboni <tholumuzi@inandi.co.za>
2024-04-10 18:01:22 +0000

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+
+}
