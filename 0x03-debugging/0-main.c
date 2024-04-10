555d9c458d742c1fd7b5a87855917a40ba96fad1
Tholumuzi Kuboni <tholumuzi@inandi.co.za>
2024-04-10 08:31:18 +0000

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
